569b35f1645f285d9401b98befb26c09b1e23780
phongnguyen2593 <phongnguyen2593@gmail.com>
2020-08-20 20:39:36 +0700

    add hw unit 03 ex 3

---
+++ Unit03/ex03/data.php
@@ -0,0 +1,23 @@
+<?php 
+	$phones = array();
+	$phones[] = array(
+		'id' => 'SP1',
+		'name' => 'Note 20 Ultra 5G',
+		'price' => "32990000"
+	);
+	$phones[] = array(
+		'id' => 'SP2',
+		'name' => 'iPhone 7 Plus 32GB',
+		'price' => "11490000"
+	);
+	$phones[] = array(
+		'id' => 'SP3',
+		'name' => 'iPad 10.2 inch Wifi (2019) 128GB',
+		'price' => "11990000"
+	);
+	$phones[] = array(
+		'id' => 'SP4',
+		'name' => 'iPhone 11 Pro Max 64GB',
+		'price' => "33990000"
+	);  
+ ?>
